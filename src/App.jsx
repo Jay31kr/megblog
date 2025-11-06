976c99084920304fbb7752461e8047c75cdb3353
Jay Kumar Gupta <112371389+Jay31kr@users.noreply.github.com>
2025-11-06 11:37:13 +0530

    Initial commit - setup React + Appwrite base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <> <div className="min-h-screen w-screen flex justify-center items-center">
+
+      <h1 className=' p-4 text-2xl text-center bg-orange-400 text-white'><span className=' text-shadow-violet-100 font-extrabold '>Hello, </span> I'm Live</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
